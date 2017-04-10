59d6a38cca9f0179fde520ccaa75a6063dc4e672
dp486x <poladinesh42@gmail.com>
2017-04-10 15:37:07 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ var express = require('express'); // Require Express module
 var app = express();
 var http = require('http').Server(app); // Http server
 var mongo = require('mongoskin'); // Require mongoskin module
-var db = mongo.db("mongodb://admin:WGAPUKDHNENINCRK@sl-us-dal-9-portal.6.dblayer.com:22547,sl-us-dal-9-portal.7.dblayer.com:22547/admin?ssl=true", {native_parser:true});
+var ca1 = [new Buffer("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURoVENDQW0yZ0F3SUJBZ0lFV09lc0VEQU5CZ2txaGtpRzl3MEJBUTBGQURCRU1VSXdRQVlEVlFRREREbHcKYjJ4aFpHbHVaWE5vTWpBd01FQjVZV2h2Ynk1amIyMHRZMk5pTWpObFltWTVPVGxqTW1JeVlUYzNNRGswWkdGbQpOalExTnpjMU1HUXdIaGNOTVRjd05EQTNNVFV4TVRFeVdoY05NemN3TkRBM01UVXdNREF3V2pCRU1VSXdRQVlEClZRUURERGx3YjJ4aFpHbHVaWE5vTWpBd01FQjVZV2h2Ynk1amIyMHRZMk5pTWpObFltWTVPVGxqTW1JeVlUYzMKTURrMFpHRm1OalExTnpjMU1HUXdnZ0VpTUEwR0NTcUdTSWIzRFFFQkFRVUFBNElCRHdBd2dnRUtBb0lCQVFEQgowM21iQm9KUlpxSnlrcEQrZUpKYjNUN3RlTXRwWjZhclJ5VGUzbENaVWE0VkFwZTJrQmxYUnZyc0tEODY3bTAzCjlsckVOQW83bkV6Nkd3MzQvZDVDL1VKNm1jL1hZaktnOEJvQXdiRi8wVENmNkkyQm1heFRZZlZ0UmR4SjUzelgKaExyMGhIdUFjQmF3RDZNcjliNVdPdWhuZ2t4VW1QR0ZWSGRMUXZyV0wzYmNBVTdUWVE3STBVcVg3Y3ZUTjFjVQpaSjV1angyaEFJcEdBVXEzQmYxR01UMFExblRTUU1kTUpuVEFJMXVGNGhFTlVwT21wYmFCOUQ1aHRhR3JLcEkwCjZxU1prSi9xZTAxZFlKUTVaUHZKUmhJNHh2OHp6MnFpL0pMZVI5NW5LZ3dJanlZUkV5blIzakRJRzB0NE9uR1kKNXRoN2RNRlJMZnVydnhMS0hLbXJBZ01CQUFHamZ6QjlNQjBHQTFVZERnUVdCQlNxWnNjUFZPM3diVVhjRmd4SQovYUxCYVZKU0xqQU9CZ05WSFE4QkFmOEVCQU1DQWdRd0hRWURWUjBsQkJZd0ZBWUlLd1lCQlFVSEF3RUdDQ3NHCkFRVUZCd01DTUF3R0ExVWRFd1FGTUFNQkFmOHdId1lEVlIwakJCZ3dGb0FVcW1iSEQxVHQ4RzFGM0JZTVNQMmkKd1dsU1VpNHdEUVlKS29aSWh2Y05BUUVOQlFBRGdnRUJBSENmKzc4UWNlMTVsVUg0QWNieFJidWVBalVuNG1xaQorTzduUThHempqVWhjYVRINzZlS2VPekg4eFdzcEdwOEx4K05ZOHQ2eWxySGdVRGlSTG9DN3kyU3JaVGd4VnRUCkVQbFJwYjNJUHpvOFY2dWxKY2tJeTgyeGMwS3hGdUJqazRuNnJFejNsRXhNUHQ3a1JlaENuUWE4REhkNlRxNTEKZnZiUXdVMUpCNkkvekN4M1ZiR1A0WTAzVWpIdDEzQnlBdXc2cDhpNHNKSGF2MEwwcS9lelo2RGZNeVRXWERFZAplV3l1VEZKM29wK1RieHpwcEZaWHhhVG55TUQ1MW5SRTNUOUIwS0UyTWNTR1dzYkNyMy8wRUpKbHB2eFZkazRJClM2WHNwQkExN3h3TDJsYisxa2REWDRJMXUzMDVUT2xxMGFGY0NJQnJrNDRNV0lJd0o3N1JmWTA9Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K",'base64')];
+var details = {
+    mongos: {
+        ssl: true,
+        sslValidate: true,
+        sslCA:ca1,
+        poolSize: 1,
+        reconnectTries:1
+    }
+};
+var db = mongo.db("mongodb://admin:WGAPUKDHNENINCRK@sl-us-dal-9-portal.6.dblayer.com:22547,sl-us-dal-9-portal.7.dblayer.com:22547/admin?ssl=true",  ['inventory'], details, {native_parser:true});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // Body parser use JSON data
 app.use(function(req,res,next){
